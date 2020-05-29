0df434ad41a2e99dba27e120b6bad23fec747e6b
Arif <arifkhan65@gmail.com>
2020-05-29 17:10:36 +0200

    Initial commit

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+print('what is your joining year? ')
+
+year=input()
+
+if (year>='2017'):
+    print('you are not eligible for bonus')
+else:
+    print('contact HR for Bonus amount 2500$')
+
+
+    print('your HR team')
